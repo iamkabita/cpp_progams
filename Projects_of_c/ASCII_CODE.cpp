78ef5d1a2b88700529c67e2aeaa887c08bd4d545
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-03-02 17:24:36 +0530

    practice some problem

---
+++ Projects_of_c/ASCII_CODE.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+	char ch;
+	cout<<"Enter a character\n";
+	cin>>ch;
+	cout<<"ASCII Code of "<<ch<<" is "<< int (ch);
+	getch();
+	return 0;	
+}
