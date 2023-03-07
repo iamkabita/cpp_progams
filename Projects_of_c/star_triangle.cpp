8a64b7adf6f8b1e46741803f2ae6ef759f74103d
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-03-07 13:09:49 +0530

    practice some problem

--- Projects_of_c/star_triangle.cpp
+++ Projects_of_c/star_triangle.cpp
@@ -4,5 +4,25 @@ using namespace std;
 
 int main()
 {
-	int 
+	int i,j,s;
+	cout<<"Enter number of row ";
+	cin>>s;
+	for(i=1;i<=s;i++)
+	{
+		for(j=1;j<=2*s-1;j++)
+		{
+			if(j>=s+1-i&&j<=s-1+i)
+		     {
+		     	cout<<"* ";
+		     }
+		     else
+		     {
+		     	cout<<"  ";
+			 }
+		}
+		cout<<"\n";
+		
+	}
+	getch();
+	return 0;
 }
