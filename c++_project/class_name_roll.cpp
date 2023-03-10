70fefcd6f8c7254a4ffe244320d62e4064fb19b3
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-03-10 17:13:46 +0530

    practice some problem

--- c++_project/class_name_roll.cpp
+++ c++_project/class_name_roll.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
 
+class student
+{
+	int roll;
+	char name[20];
+	public :
+		void getdata(void);
+		void display(void)
+		{
+			cout<<"Your Name : "<<name<<endl;
+			cout<<"Your roll no. : "<<roll<<endl;
+		}
+};
+void student::getdata(void)
+{
+	cout<<"Enter your name ";
+	cin>>name;
+	cout<<"Enter your roll no. ";
+	cin>>roll;
+}
+int main()
+{
+	student data;
+	data.getdata();
+	data.display();
+	getch();
+	return 0;
+}
