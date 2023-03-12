e37347e71b5244cd6b3778b6e86b77113b62351d
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-03-12 21:18:51 +0530

    practice some problem

--- c++_project/copy_construction.cpp
+++ c++_project/copy_construction.cpp
@@ -2,3 +2,45 @@
 #include<conio.h>
 using namespace std;
 
+class Dummy
+{
+	private:
+		int a,b;
+		int *p;
+	public:
+		Dummy()
+		{
+			p = new int;
+		}
+		void setData (int x, int y,int z)
+		{
+			a = x; b = y;
+			*p = z;
+			
+		}
+		void showData()
+		{
+			cout<<"\na = "<<a<<"\nb = "<<b;
+		}
+		
+		Dummy(Dummy & d)
+		{
+			a = d.a;  b = d.b;
+			p = new int;
+			*p = *(d.p);
+		}
+		~Dummy()
+		{
+			delete p;
+		}
+};
+int main()
+{
+	Dummy d1;
+	d1.setData(3,4,5);
+	Dummy d2 = d1;
+	d2.showData();
+	getch();
+	return 0;
+}
+
