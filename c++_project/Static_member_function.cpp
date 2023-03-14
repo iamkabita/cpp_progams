090be5308f6ef94a208fb6b01688be740168fbcb
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-03-14 19:09:29 +0530

    practice some problem

---
+++ c++_project/Static_member_function.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class Account
+{
+	int balance;      // instance member function
+	static float ROI; // static member variable
+	public:
+		void setBalance(int b)
+		{
+			balance = b;
+		}
+		static void setRoi(float r) //static member function
+		{
+			ROI = r;
+		}
+};
+float Account :: ROI=10;
+int main()
+{
+	Account :: setRoi (float(10));
+	cout<<"ROI = "<<setRoi;
+	getch();
+	return 0;
+}
