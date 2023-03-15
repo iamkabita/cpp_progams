7c4967e4f79f56095474c3a3d669570aab5f4077
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-03-15 17:59:39 +0530

    practice some problem

---
+++ Projects_of_c/Recursion_power_Number.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+// Write a c++ program to calculate power of a number (Recursion).
+
+int power(int n, int p)
+{
+	if(p==0)
+	{
+		return 1;
+	}
+	return (n*(power(n,(p-1))));
+}
+int main()
+{
+	int n,p,i,sum=1;
+	cout<<"Enter a number and power of number"<<endl;
+	cin>>n>>p;
+	cout<<p<<" Power of "<<n<<" is "<<power(n,p);
+	return 0;
+}
