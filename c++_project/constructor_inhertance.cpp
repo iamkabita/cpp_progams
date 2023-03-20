262089ed3db36b1ec05b5ad544db122de3871b22
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-03-20 22:10:03 +0530

    practice some problem

--- c++_project/constructor_inhertance.cpp
+++ c++_project/constructor_inhertance.cpp
@@ -2,4 +2,27 @@
 #include<conio.h>
 using namespace std;
 
-
+class A
+{
+	int a;
+	public:
+		A(int k)
+		{
+			a = k;
+		}
+};
+class B:public A
+{
+	int b;
+	public:
+		B(int x, int y):A(x)
+		{
+			b = y;	
+		}
+		
+};
+int main()
+{
+	B obj(2,3);
+	
+}
