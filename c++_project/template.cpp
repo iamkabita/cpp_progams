0b7493f0380efe4a7c47e8ef3b060086d4899518
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-03-23 22:18:39 +0530

    practice some problem

--- c++_project/template.cpp
+++ c++_project/template.cpp
@@ -2,4 +2,22 @@
 #include<conio.h>
 using namespace std;
 
-
+template <class X>
+X Big(X a, X b)
+{
+	if(a>b)
+	{
+		return a;
+	}
+	else
+	{
+		return b;
+	}
+}
+int main()
+{
+	cout<<Big(5,7)<<endl;
+	cout<<Big(4.3 , 5.4);
+	getch();
+	return 0;
+}
