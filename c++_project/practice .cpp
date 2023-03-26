a619a31e6007623c488d3e6afb4c40a336a53704
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-03-26 22:47:28 +0530

    practice some problem

--- c++_project/practice .cpp
+++ c++_project/practice .cpp
@@ -2,4 +2,18 @@
 #include<conio.h>
 using namespace std;
 
+int main()
+{
+	int i,j,r;
+	cout<<"Enter number of row  ";
+	cin>>r;
+	for(i=1;i<=r;i++)
+	{
+		for(j=1;j<=2*r-1;j++)
+		{
+			if(i==4||j==4||i=)			
+		}
+	}
+	return 0;
+}
 
