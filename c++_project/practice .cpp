4517b00f56cdf43eb61268668edaa57c5d455222
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-03-26 22:55:53 +0530

    practice some problem

--- c++_project/practice .cpp
+++ c++_project/practice .cpp
@@ -4,15 +4,25 @@ using namespace std;
 
 int main()
 {
-	int i,j,r;
+	int i,j,r,k;
 	cout<<"Enter number of row  ";
 	cin>>r;
 	for(i=1;i<=r;i++)
 	{
 		for(j=1;j<=2*r-1;j++)
 		{
-			if(i==4||j==4||i=)			
+			k=1;
+			if(j>=r+i&&j<=r-i)
+			{
+				cout<<k<<" ";
+			}
+			else
+			{
+				cout<<"  ";
+			}
+			k++;			
 		}
+		cout<<endl;
 	}
 	return 0;
 }
