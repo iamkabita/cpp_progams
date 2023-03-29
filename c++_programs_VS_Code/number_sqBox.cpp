0351ce805b04c069df8a4758fb9142c167782f09
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-03-29 10:47:11 +0530

    practice some problem

--- c++_programs_VS_Code/number_sqBox.cpp
+++ c++_programs_VS_Code/number_sqBox.cpp
@@ -5,16 +5,15 @@ using namespace std;
 int main()
 {
     int i,j,r;
-    cout<<"Enter row number"<<endl;
+    cout<<"Enter number of row"<<endl;
     cin>>r;
     for(i=1;i<=r;i++)
     {
-        for(j=1;j<=i;j++)
+        for(j=1;j<=r;j++)
         {
-            cout<<r-i+j<<" ";
+            cout<<j+i<<"  ";
         }
         cout<<endl;
     }
     return 0;
 }
-
