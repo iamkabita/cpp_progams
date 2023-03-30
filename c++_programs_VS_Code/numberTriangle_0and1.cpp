e1b7576e6f782a159d2df21ee0bcdbc3ba3c7ad6
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-03-30 22:28:31 +0530

    practice some problem

--- c++_programs_VS_Code/numberTriangle_0and1.cpp
+++ c++_programs_VS_Code/numberTriangle_0and1.cpp
@@ -4,5 +4,22 @@ using namespace std;
 
 int main()
 {
-    
+    int i,j,r;
+    cout<<"Enter number of rows";
+    cin>>r;
+    for(i=1;i<=r;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            if(j % 2==0)
+            {
+                cout<<"0 ";
+            }
+            else{
+                cout<<"1 ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
 }
