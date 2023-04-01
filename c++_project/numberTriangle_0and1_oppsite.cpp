766b34654581bda16c76ca960c735b461d357ec7
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-04-01 12:34:47 +0530

    practice some problem

--- c++_project/numberTriangle_0and1_oppsite.cpp
+++ c++_project/numberTriangle_0and1_oppsite.cpp
@@ -5,5 +5,38 @@ using namespace std;
 int main()
 {
 	int i,j,k,n;
-	
+	cout<<"Enter number of rows"<<endl;
+	cin>>n;
+	for(i=n;i>=1;i--)
+	{
+		k=1;
+		for(j=1;j<=i;j++)
+		{
+			if(i%2==0)
+			{
+				if(k==1)
+				{
+					k--;
+					cout<<k<<" ";
+				}
+				else
+				{
+					k++;
+					cout<<k<<" ";
+				}
+			}
+			else if(k==1)
+			{
+				cout<<k<<" ";
+				k--;
+			}
+			else
+			{
+				cout<<k<<" ";
+				k++;
+			}
+		}
+		cout<<endl;
+	}
+	return 0;
 }
