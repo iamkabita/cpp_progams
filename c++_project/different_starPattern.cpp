7ef93c40c85bcb1a503bf97e81011eb5d4b838d9
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-04-02 09:34:21 +0530

    practice some problem

--- c++_project/different_starPattern.cpp
+++ c++_project/different_starPattern.cpp
@@ -5,38 +5,20 @@ using namespace std;
 int main()
 {
 	int i,j,n;
-	for(i=1;i<=12;i++)
+	cout<<"Enter number of rows";
+	cin>>n;
+	for(i=1;i<=n;i++)
 	{
-		for(j=1;j<=12;j++)
+	    if(i%2 !=0)
 		{
-			if(i%2 !=0)
-			{
-				if(i==1||i==5||i==9)
-				{
-					if(j==12)
-					{
-						cout<<" ";
-					}
-					else
-					{
-						cout<<"*";
-					}
-				}
-				else if(j==1)
-				{
-					cout<<" ";
-				}
-				else
-				{
-					cout<<"*";
-				}
-			}
-			else
-			{
-				cout<<" ";
-			}
+			cout<<"**********";
+		}
+		else
+		{
+		    cout<<" **********";
 		}
 		cout<<endl;
+		cout<<endl;			
 	}
 	return 0;
 }
