4d615b65661303dd0151bc8d6303e5ff148ce29b
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-04-04 22:42:31 +0530

    practice some problem

--- c++_project/circle.cpp
+++ c++_project/circle.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
 #include<conio.h>
+#include<graphics.h>
 using namespace std;
 int main()
 {
-	
+	int gd=0,gm;
+	initgraph(&gd,&gm,"");
+	circle(300,200,70);
+	getch();
+	closegraph();
+	return 0;
 }
