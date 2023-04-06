88ce101b93b28b2f6305de04e9dd3933d6447f6f
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-04-06 18:57:19 +0530

    practice some problem

--- c++_project/journey.cpp
+++ c++_project/journey.cpp
@@ -155,5 +155,280 @@ int main()
 		}
 		cout<<endl;
 	}
+	
+	
+	system("cls");
+	
+	for(i=1;i<=30;i++)
+	{
+		for(j=1;j<=24;j++)
+		{
+			if(i==1||j==1||i==7||i==13||i==19||i==25)
+			{
+				cout<<"  ";
+			}
+			else if(i==2||i==3||i==4||i==5||i==6)
+			{
+				if(j==2||j==4||j==6||j==17)
+				{
+					cout<<"* ";
+				}
+				else if(i==2||i==6)
+				{
+					if(j==3||j==7||j==8||j==10||j==11||j==12||j==13)
+					{
+						cout<<"* ";
+					}
+					else if(i==2)
+					{
+						if(j==15||j==16||j==17||j==18||j==19)
+						{
+							cout<<"* ";
+						}
+						else
+						{
+							cout<<"  ";
+						}
+					}
+				}
+				else if(i==4)
+				{
+					if(j==3||j==7||j==8||j==11)
+					{
+						cout<<"* ";
+					}
+				}
+				else if(i==3)
+				{
+					if(j==10||j==13)
+					{
+						cout<<"* ";
+					}
+					else{
+						cout<<"  ";
+					}
+				}
+				else if(i==5)
+				{
+					if(j==10||j==12)
+					{
+						cout<<"* ";
+					}
+					else{
+						cout<<"  ";
+					}
+					
+				}
+				else{
+					cout<<"  ";
+				}
+				
+			}
+			else if(i==8||i==9||i==10||i==11||i==12)
+			{
+				if(j==7||j==9||j==11)
+				{
+					cout<<"* ";
+				}
+				else if(i==8)
+				{
+					if(j==8||j==12||j==13)
+					{
+						cout<<"* ";
+					}
+					else{
+						cout<<"  ";
+					}
+				}
+				else if(i==10)
+				{
+					if(j==12||j==13)
+					{
+						cout<<"* ";
+					}
+					else{
+						cout<<"  ";
+					}
+				}
+				else if(i==12)
+				{
+					if(j==8)
+					{
+						cout<<"* ";
+					}
+					else{
+						cout<<"  ";
+					}
+				}
+				else{
+					cout<<"  ";
+				}
+			}
+			else if(i==14||i==15||i==16||i==17||i==18)
+			{
+				if(j==3||j==7||j==10||j==12||j==17)
+				{
+					cout<<"* ";
+				}
+				else if(i==14||i==18)
+				{
+					if(j==13||j==14||j==15||j==20)
+					{
+						cout<<"* ";
+					}
+					else if(i==18)
+					{
+						if(j==4||j==5||j==8||j==9)
+						{
+							cout<<"* ";
+						}
+						else{
+							cout<<"  ";
+						}
+					}
+					else{
+						cout<<"  ";
+					}
+				}
+				else if(i==15||i==17)
+				{
+					if(j==19)
+					{
+						cout<<"* ";
+					}
+					else{
+						cout<<"  ";
+					}
+				}
+				else if(i==16)
+				{
+					if(j==18)
+					{
+						cout<<"* ";
+					}
+					else{
+						cout<<"  ";
+					}
+				}
+				else{
+					cout<<"  ";
+				}
+			}
+			else if(i==20||i==21||i==22||i==23||i==24)
+			{
+				if(j==5||j==9||j==11||j==13)
+				{
+					cout<<"* ";
+				}
+				else if(i==20||i==21||i==22||i==24)
+				{
+					if(j==15)
+					{
+						cout<<"* ";
+					}
+					
+			        else if(i==20||i==22)
+				    {
+					    if(j==6||j==7||j==14)
+					       {
+						      cout<<"* ";
+					       }
+						else if(i==20)
+				          {
+					         if(j==10)
+					           {
+						          cout<<"* ";
+					           }
+					         else{
+						           cout<<"  ";
+					        }
+				         }		
+					}
+				}
+					
+			    else if(i==23)
+				   {
+					   if(j==14)
+					     {
+						     cout<<"* ";
+					     }
+					    else{
+						     cout<<"  ";
+					     }
+				    }
+				else if(i==24)
+				{
+					if(j==10)
+					{
+						cout<<"* ";
+					}
+				}
+			}
+			else if(i==26||i==27||i==28||i==29||i==30)
+			{
+				if(j==3||j==13||j==15||j==17||j==23)
+				{
+					cout<<"* ";
+				}
+				else if(i==26||i==30)
+				{
+					if(j==4||j==5||j==7||j==11)
+					{
+						cout<<"* ";
+					}
+					else if(i==26)
+					{
+						if(j==14||j==18||j==22)
+						{
+							cout<<"* ";
+						}
+						else{
+							cout<<"  ";
+						}
+					}
+					else{
+						cout<<"  ";
+					}
+				}
+				else if(i==27)
+				{
+					if(j==8||j==10||j==19||j==21)
+					{
+						cout<<"* ";
+					}
+					else{
+						cout<<"  ";
+					}
+				}
+				else if(i==28)
+				{
+					if(j==4||j==5||j==9||j==14||j==20)
+					{
+						cout<<"* ";
+					}
+					else{
+						cout<<"  ";
+					}
+				}
+				else if(i==29)
+				{
+					if(j==8||j==10)
+					{
+						cout<<"* ";
+					}
+					else{
+						cout<<"  ";
+					}
+				}
+				else{
+					cout<<"  ";
+				}
+			}
+			else{
+				cout<<"  ";
+			}
+		}
+		cout<<endl;
+	}
 	return 0;
 }
