02860d76ba193eafb6cf7dba4dc4c09ee86efb7e
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-04-10 22:51:57 +0530

    practice some problem

--- Projects_of_c/Is_number_prime.cpp
+++ Projects_of_c/Is_number_prime.cpp
@@ -2,4 +2,25 @@
 #include<conio.h>
 using namespace std;
 
-
+int main()
+{
+	int n,i;
+	cout<<"Enter a number ";
+	cin>>n;
+	for(i=2;i<=n/2;i++)
+	{
+		if(n % i==0)
+		{
+			break;
+		}
+	}
+	if(i==(n/2)+1)
+	{
+		cout<<"Prime number";
+	}
+	else
+	{
+		cout<<"Not Prime number";
+	}
+	return 0;	
+}
