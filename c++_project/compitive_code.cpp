6c791e53d8b3b2bd448e4f3ed1696cceab3d7266
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-04-11 18:39:04 +0530

    practice some problem

---
+++ c++_project/compitive_code.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+	int n,i,j,k,r=0;
+	cout<<"Enter a number ";
+	cin>>n;
+	for(i=1;i<=2*n-1;i++)
+	{
+		if(i<=n)
+		{
+			r++;
+		}
+		else
+		{
+			r--;
+		}
+		
+		k=n;
+		for(j=1;j<=2*n-1;j++)
+		{
+			if(j>=r&&j<=8-r)
+			{
+				cout<<k;
+			}
+			else
+			{
+				if(j>n)
+				{
+					k++;
+					cout<<k;
+				}
+				else
+				{
+					cout<<k;
+					k--;
+				}
+			}
+		}
+		cout<<endl;
+	}
+	return 0;
+}
