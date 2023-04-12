65864734fd27dabf57d41f561d0b635134b02d65
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-04-12 13:35:15 +0530

    practice some problem

---
+++ c++_project/rotate_onePosition.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+void rotateRight(int A[],int size)
+{
+	int i,temp;
+	temp = A[size-1];
+	for(i=size-2;i>=0;i--)
+	{
+		A[i+1] = A[i];
+	}
+	A[0]=temp;
+}
+int main()
+{
+	int i,a[]={45,89,41,90,78,12,93,33,57,29,56};
+	for(i=0;i<=9;i++)
+	{
+		cout<<a[i]<<" ";
+	}
+	cout<<endl;
+	rotateRight(a,10);
+	for(i=0;i<=9;i++)
+	{
+		cout<<a[i]<<" ";
+	}
+	return 0;
+}
