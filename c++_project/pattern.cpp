dada12aecfa3ac2c3b3cf9787dec0f9f50bb7a10
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-04-14 21:56:57 +0530

    practice some problem

--- c++_project/pattern.cpp
+++ c++_project/pattern.cpp
@@ -2,4 +2,31 @@
 #include<conio.h>
 using namespace std;
 
-
+int main()
+{
+	int i,j,k=0;
+	for(i=1;i<=9;i++)
+	{
+		if(i<=5)
+		{
+			k++;
+		}
+		else
+		{
+			k--;
+		}
+		for(j=1;j<=9;j++)
+		{
+			if(j<=6-k||j>=4+k)
+			{
+				cout<<"* ";
+			}
+			else
+			{
+				cout<<"  ";
+			}
+		}
+		cout<<endl;
+	}
+	return 0;
+}
